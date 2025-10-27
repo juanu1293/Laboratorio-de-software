f4be733ba744084f317d0b88cde69cdf700e5012
Dav1nch00 <147347622+Dav1nch00@users.noreply.github.com>
2025-10-26 20:40:47 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const verifyToken = require("./middleware/authMiddleware");
 const dotenv = require("dotenv");
 
+
 dotenv.config();
 
 const app = express();
@@ -25,6 +26,20 @@ app.get("/api/perfil", verifyToken, (req, res) => {
     mensaje: "Accediste a un endpoint protegido",
     usuario: req.user,
   });
+});
+
+const locationRoutes = require("./routes/locationRoutes");
+app.use("/api/location", locationRoutes);
+
+const flightRoutes = require("./routes/flightRoutes");
+app.use("/api/flights", flightRoutes);
+
+
+// Puerto
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
 const locationRoutes = require("./routes/locationRoutes");
