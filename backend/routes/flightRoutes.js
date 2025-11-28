33efccc747d20740a240cd2b125b3d74bb2bdc33
juanu1293 <147016986+juanu1293@users.noreply.github.com>
2025-11-28 02:16:58 -0500

    Update flightRoutes.js

--- backend/routes/flightRoutes.js
+++ backend/routes/flightRoutes.js
@@ -1,10 +1,15 @@
 // routes/flightRoutes.js
 const express = require("express");
 const router = express.Router();
-const { createFlight } = require("../controllers/flightController");
+const { createFlight, checkFlightSeats, updateFlight, getFlights } = require("../controllers/flightController"); // Asegúrate de importar las nuevas funciones
 const verifyToken = require("../middleware/authMiddleware");
 
-// Crear un vuelo (requiere autenticación)
+// Rutas existentes
+router.get("/", getFlights); // <--- ESTA ES LA NUEVA (Obtener lista real)
 router.post("/", verifyToken, createFlight);
+
+// NUEVAS RUTAS
+router.get("/:id/check-seats", verifyToken, checkFlightSeats); // Verificar asientos
+router.put("/:id", verifyToken, updateFlight); // Actualizar vuelo
 
 module.exports = router;
